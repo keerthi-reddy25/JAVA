a479bd4f584027437a358762017bca877b16f507
keerthi-reddy25 <kamireddykeerthireddy@gmail.com>
2025-12-21 09:29:04 +0530

    Add files via upload

---
+++ ExampleForLinkedHashSet.java
@@ -0,0 +1,19 @@
+package basic;
+
+import java.util.LinkedHashSet;
+
+public class ExampleForLinkedHashSet {
+
+	public static void main(String[] args) {
+		LinkedHashSet<Integer> lhs=new LinkedHashSet<Integer>();
+		lhs.add(10);
+		lhs.add(20);
+		lhs.add(30);
+		lhs.add(40);
+		System.out.println(lhs);
+		for(Object i:lhs) {
+			System.out.println(i);
+		}
+	}
+
+}
