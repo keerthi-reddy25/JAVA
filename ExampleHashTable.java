5084c6e80ab98a706bf57dc96e7e30247479740e
keerthi-reddy25 <kamireddykeerthireddy@gmail.com>
2025-12-21 14:02:42 +0530

    Add files via upload

---
+++ ExampleHashTable.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Hashtable;
+
+public class ExampleHashTable {
+
+	public static void main(String[] args) {
+		Hashtable ht=new Hashtable();
+		ht.put("Name","Nami");
+		ht.put("Email","nami@gmail.com");
+		ht.put("Age",21);
+		System.out.println(ht);
+
+	}
+
+}
